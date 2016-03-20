cad27835414085471925a180116fc3b0a9c85bde
Robby Wagner <robby.wagner1991@web.de>
2016-03-20 18:37:34 +0100

    Added more functions

--- lib/wg_lib.py
+++ lib/wg_lib.py
@@ -1,37 +1,57 @@
 import logging as logging
 import subprocess
-
-from conf import config as cfg
-
+import os
 
 class WgLibException(Exception):
     pass
 
-class DataQuerrierLib(self):
+class DataQuerrierLib():
     pass
 
-class WgLib()
-    def exec_nas_cmd(self, cmd):
-        user, password = cfg.Identifier.get_identifications('NAS')
-
-        cmd = ('sshpass -p ' + password + ' ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -p 22 ' +
-               user + '@wgserv' + ' ' + '\'' + cmd + '\'')
-
-        return exec_cmd(cmd)
-
-    def exec_cmd(cmd):
+class WgLib():
+    def get_dir_list(self, path):
         '''
+        Return a list paths containing the initial path and appended all
+        contained files. With get_dir_list().split('/')[-1] the actual contained
+        files can be retrieved.
+
+        :param path:
+        :return:
+        '''
+        dirs = []
+        ret = os.listdir(path)
+
+        for _ in ret:
+            dir = os.path.join(path, _)
+            dirs.append(dir)
+
+        return dirs
+
+    def exec_cmd(self, cmd):
+        '''
+        Executes a command after checking if the command is empty.
+        The return is always a string! The returncode is logged
+        as Debug.
 
         :param cmd:
         :return:
         '''
-        if not cmd:
-            logger('INFO', 'No cmd given. Nothing happend.')
+        if cmd is None:
+            raise WgLibException('No command given.')
 
-        return subprocess.check_output(cmd)
+        log_msg = 'Executing: ' + str(cmd)
+        self.logger('INFO', log_msg)
+
+        try:
+            ret = subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
+        except subprocess.CalledProcessError as e:
+            self.logger('DEBUG', e)
+
+        return string(ret)
 
     def logger(type, msg):
         '''
+        Logs in INFO or Debug after checking is the msg is not empty.
 
         :param self:
         :param type:
@@ -42,8 +62,8 @@ class WgLib()
         if type is 'INFO':
             logger.info(msg)
         elif type is 'DEBUG':
-            logger.warning(msg)
-        else type is 'warning':
+            logger.debug(msg)
+        else:
             raise WgLibException('No suitable type given. Please use only INFO and DEBUG.')
 
         return
